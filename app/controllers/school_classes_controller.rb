46e0804f03225d23debf9507652800874940e5a7
Sébastien Mercier-Bellevue <sebastien.mercierbellevue@gmail.com>
2023-10-05 17:31:40 +0200

    formulaire creation cours avec verification

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -8,21 +8,27 @@ class SchoolClassesController < ApplicationController
   def create
     
     @schoolclass = SchoolClass.new(schoolclass_params)
-    @schoolclass.school = @school
-
-    if @schoolclass.save
-       redirect_to schools_path , notice: "Votre cours a bien été crée"
-     else
-       render :new, status: :unprocessable_entity
-     end
+    @schoolclass.school = @school 
+    @schoolclassesroom = SchoolClass.where(beginning_of_time: @schoolclass.beginning_of_time, end_of_time: @schoolclass.end_of_time, room_number: @schoolclass.room_number)
+    @schoolclassestea = SchoolClass.where(beginning_of_time: @schoolclass.beginning_of_time, end_of_time: @schoolclass.end_of_time, teacher_name: @schoolclass.teacher_name)
     
+    if @schoolclassesroom.any?
+      redirect_to new_school_school_class_path(:school_id => @school.id) ,notice: "Cette salle est déja réservé à cette horaire"
+    elsif @schoolclassestea.any?
+      redirect_to new_school_school_class_path(:school_id => @school.id) ,notice: "Ce professeur est déja pris à cette horaire"
+    else 
+      if @schoolclass.save
+        redirect_to schools_path , notice: "Votre cours a bien été crée"
+      else
+        render :new, status: :unprocessable_entity
+      end
+    end
   end
 
   private
 
   def schoolclass_params
-    params.require(:school_class).permit(:type_of_dance)
-      #, :level, :end_of_time, :beginning_of_time, :day_of_week, :room_number, :teacher_name)
+    params.require(:school_class).permit(:type_of_dance, :level, :end_of_time, :beginning_of_time, :day_of_week, :room_number, :teacher_name)
   end
 
   def set_school
--- app/models/school_class.rb
+++ app/models/school_class.rb
@@ -1,3 +1,11 @@
 class SchoolClass < ApplicationRecord
   belongs_to :school
+  validate :check_dates
+  validates :beginning_of_time, :end_of_time, presence: true
+
+  def check_dates
+    if (beginning_of_time > end_of_time) || (beginning_of_time == end_of_time)
+      errors.add(:end_of_time, message: "Ne peut pas être avant, ou égale à la date de début")
+    end
+  end
 end
