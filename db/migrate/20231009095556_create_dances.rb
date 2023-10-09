905a6e5be4947e5079de3d67597b65ecd0e7f55a
Sébastien Mercier-Bellevue <sebastien.mercierbellevue@gmail.com>
2023-10-09 14:36:44 +0200

    reverse table dances

--- db/migrate/20231009095556_create_dances.rb
+++ db/migrate/20231009095556_create_dances.rb
@@ -1,8 +1,7 @@
 class CreateDances < ActiveRecord::Migration[7.0]
   def change
     create_table :dances do |t|
-      t.string :first_name
-      t.string :last_name
+      t.string :fullname
 
       t.timestamps
     end
