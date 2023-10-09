731b5c5cc066b500ed04fa378c1081fbc9ed0118
Sébastien Mercier-Bellevue <sebastien.mercierbellevue@gmail.com>
2023-10-09 14:34:13 +0200

    modification table dances

--- db/migrate/20231009095556_create_dances.rb
+++ db/migrate/20231009095556_create_dances.rb
@@ -1,7 +1,8 @@
 class CreateDances < ActiveRecord::Migration[7.0]
   def change
     create_table :dances do |t|
-      t.string :fullname
+      t.string :first_name
+      t.string :last_name
 
       t.timestamps
     end
