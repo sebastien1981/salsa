fefebdbd9a61785e6b65f4745ba3bd567c366a8c
Sébastien Mercier-Bellevue <sebastien.mercierbellevue@gmail.com>
2023-10-10 10:33:52 +0200

    modification de teacher

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,2 +1,4 @@
 class Teacher < ApplicationRecord
+  has_and_belongs_to_many :dances
+  validates :first_name, :last_name, :date_of_birth, :phone_number, :address_mail, :specialty, presence: true
 end
