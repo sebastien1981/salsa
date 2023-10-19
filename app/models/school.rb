1f1d067f03d18d1c0c7f7f0647bccdee74fa4141
sebastien mercier-bellevue <sebastien.mercierbellevue@gmail.com>
2023-10-19 17:14:12 +0200

    ajout des regles pour la creation d'une ecole

--- app/models/school.rb
+++ app/models/school.rb
@@ -4,5 +4,11 @@ class School < ApplicationRecord
   has_many :users, through: :user_schools
   has_many :school_classes
 
-  validates :name,:address,:type_of_structure,:approval_number,:phone_number,:address_mail_pdt,:phone_number_pdt,  presence: true
+  VALID_EMAIL_REGEX= /^(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})$/i
+
+  validates :name,:address,:type_of_structure,:approval_number,:phone_number,:address_mail_pdt,:phone_number_pdt,:number_room,  presence: true
+  validates :number_room, numericality: { only_integer: true,greater_than_or_equal_to: 1 }
+  validates :address_mail_pdt , presence: true,format:{with:VALID_EMAIL_REGEX,multiline:true}
+  #uniqueness:{case_sensetive:false},
+  validates :phone_number,:phone_number_pdt, length: { minimum: 10 }
 end
