f230427ec2e13d14db40202a72898748b101d026
sebastien mercier-bellevue <sebastien.mercierbellevue@gmail.com>
2023-10-27 15:04:24 +0200

    teacher

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -3,7 +3,7 @@ class Teacher < ApplicationRecord
   has_many :dances, through: :teacher_dances, dependent: :destroy
 
   VALID_EMAIL_REGEX= /^(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})$/i
-  validates :first_name, :last_name, :date_of_birth,  :specialty, presence: true
+  validates :first_name, :last_name, :date_of_birth,:dance_ids, presence: true
   validates :address_mail , presence: true,format:{with:VALID_EMAIL_REGEX,multiline:true}
   #uniqueness:{case_sensetive:false},
   validates :phone_number, presence: true,length: { minimum: 10 }
